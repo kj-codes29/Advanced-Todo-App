77dd0c783de13734d5710a0c5a6cc4ea0513f4e1
Jendry Rodriguez <jendryrodriguez@Jendrys-MacBook-Pro.local>
2023-07-09 18:27:42 +0200

    refactored modal styling

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -8,6 +8,9 @@ type modalProps = {
     addTodoNote: any
 }
 
+const inputStyle = "bg-gray-600 border border-gray-500 text-gray-900 placeholder-gray-400 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+const labelStyle = "block mb-2 text-sm font-medium text-white"
+
 const TodoModal = ({isModalHidden, setIsModalHidden, addTodoNote}: modalProps) => {
     const modalRef = useRef<HTMLDivElement>(null);
 
@@ -35,10 +38,10 @@ const TodoModal = ({isModalHidden, setIsModalHidden, addTodoNote}: modalProps) =
     <div ref={modalRef} className="translate-y-1/2 translate-x-full w-full max-w-md max-h-full z-10">
 
         {/* <!-- Modal content --> */}
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div className="relative bg-gray-700 rounded-lg shadow">
 
             {/* Close mode button */}
-            <button onClick={() => hideModal()} className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+            <button onClick={() => hideModal()} className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-600 hover:text-white rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="authentication-modal">
                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -47,19 +50,19 @@ const TodoModal = ({isModalHidden, setIsModalHidden, addTodoNote}: modalProps) =
             {/* --------- */}
 
             <div className="px-6 py-6">
-                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Create Todo Note</h3>
+                <h3 className="mb-4 text-xl font-medium text-white">Create Todo Note</h3>
 
                 {/* create from */}
                 <div className="space-y-6">
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                        <input type="text" name="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="My new note" required/>
+                        <label className={labelStyle}>Title</label>
+                        <input type="text" name="title" className={inputStyle} placeholder="My new note" required/>
                     </div>
                     <div>
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                        <input type="text" name="description" placeholder="Some description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"/>
+                        <label className={labelStyle}>Description</label>
+                        <input type="text" name="description" placeholder="Some description" className={inputStyle}/>
                     </div>
-                    <button onClick={() => createNote({id: uuidv4(), title: 'This is the title', description: "some stuff"})} className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Note</button>
+                    <button onClick={() => createNote({id: uuidv4(), title: 'This is the title', description: "some stuff"})} className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create Note</button>
                 </div>
                 {/* ------ */}
             </div>
