028c12dac5230b51f9baa47b37a6b66220a80eda
Jendry Rodriguez <jendryrodriguez@Jendrys-MacBook-Pro.local>
2023-07-09 09:13:03 +0200

    added icons and styling to dropdown

--- src/components/TodoNote.tsx
+++ src/components/TodoNote.tsx
@@ -1,8 +1,15 @@
 import { RiMore2Fill } from 'react-icons/ri'
+import { LuFileSpreadsheet, LuFileEdit} from 'react-icons/lu';
+import { TiDocumentDelete} from 'react-icons/ti';
 import { Todo } from '../models/Todo'
 import { useState } from 'react'
 
-const dotSize = 22
+const iconSize: number = 22
+const options= [
+  {name:'View', icon: <LuFileSpreadsheet size={iconSize}/>}, 
+  {name:'Edit', icon: <LuFileEdit size={iconSize}/>}, 
+  {name:'Delete', icon: <TiDocumentDelete size={iconSize}/>}
+]
 
 const TodoNote: React.FC<Todo> = ({title, description}) => {
   const [isMenuHidden, setIsMenuHidden] = useState<Boolean>(true)
@@ -25,16 +32,21 @@ const TodoNote: React.FC<Todo> = ({title, description}) => {
         {/* Note options */}
         <div className='relative inline-block text-left'>
           <button onClick={ () => toggleMenu()} type='button' id="menu-button" aria-expanded="true" aria-haspopup="true" className='duration-100 ease-in hover:text-[#F5EFE7]'>
-            <RiMore2Fill size={dotSize}/>
+            <RiMore2Fill size={iconSize}/>
           </button>
 
           {/* Drop Down Menu */}
-          <div className={"absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none " + (isMenuHidden? "hidden": "" )} role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
-            <div className="py-1" role="none">
-              <a className="text-gray-700 block px-4 py-2 text-sm">View</a>
-              <a className="text-gray-700 block px-4 py-2 text-sm">Edit</a>
-              <a className="text-gray-700 block px-4 py-2 text-sm">Delete</a>
-            </div>
+          <div className={"absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[#D8C4B6] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none " + (isMenuHidden? "hidden": "" )} role="menu" aria-orientation="vertical" aria-labelledby="menu-button">
+            <ul className="">
+              {options.map(option => {
+                return (
+                <li className="flex justify-between px-4 py-2 rounded-md text-[#213555] text-sm hover:bg-[#4F709C] hover:text-[#D8C4B6]">
+                  <span>{option.name}</span>
+                   <span>{option.icon}</span>
+                </li>
+                  )
+              })}
+            </ul>
           </div>
 
         </div>
